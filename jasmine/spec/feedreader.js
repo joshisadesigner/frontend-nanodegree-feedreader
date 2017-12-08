7c9452f33c06ea816f4dc37d1abb2111bb013fff
Josh <joshisadesigner@gmail.com>
2017-12-08 12:18:11 -0600

    Remove TODO's

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -28,7 +28,7 @@ $(function() {
         
         let array = allFeeds;
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          * (Project Details #8)
@@ -43,7 +43,7 @@ $(function() {
             }
         });
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          * (Project Details #9)
@@ -59,14 +59,14 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "The menu" (Project Details #10)*/
+    /* Write a new test suite named "The menu" (Project Details #10)*/
     describe(`The menu`, function() {
         /* Create a variable for the body element of the document */
         let documentBody = $('body');
         /* Create a variable for the menu icon on the html header */
         let menuIcon = $('.menu-icon-link');
 
-        /* TODO: Write a test that ensures the menu element is
+        /* Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
@@ -78,7 +78,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that ensures the menu changes
+        /* Write a test that ensures the menu changes
          * visibility when the menu icon is clicked. This test
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
@@ -97,10 +97,10 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" (Project Details #13) */
+    /* Write a new test suite named "Initial Entries" (Project Details #13) */
     describe(`Initial Entries`, function() {
 
-        /* TODO: Write a test that ensures when the loadFeed
+        /* Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -118,10 +118,10 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" (Project Details #15) */
+    /* Write a new test suite named "New Feed Selection" (Project Details #15) */
     describe(`New Feed Selection`, function() {
         
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          * (Project Details #16)
